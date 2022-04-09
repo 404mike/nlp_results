45ce4109d4e5ea43a4f34c1cc5bf13038c2eb0b7
Mike Jones <michaeljones401@gmail.com>
2022-04-09 15:08:48 +0100

    updating ingest script

--- ingest.php
+++ ingest.php
@@ -5,39 +5,64 @@ class IngestData {
   public function __construct()
   {
     $this->ingestJournals();
-    // $this->ingestNewspapers();
+    $this->ingestNewspapers();
   }
 
   private function ingestJournals()
   {
     $files = glob('journals/data/*.{json}', GLOB_BRACE);
-    foreach($files as $file) {
-      $this->formatJournalsData($file);
+    $numFiles = count($files);
+    foreach($files as $key => $file) {
+
+      if($key == 0) $divKey = 1;
+      else $divKey = $key;
+
+      $remaining = round(($divKey / $numFiles) * 100,2);
+      echo "Running file Journal - $key out of $numFiles ($remaining%)\n";
+
+      $progress = $this->checkProgress("journal",$key);
+
+      if($progress == 0) {
+        $this->formatJournalsData($file);
+        $this->updateProgress("journal",$key);
+      }
     }
   }
 
   private function ingestNewspapers()
   {
     $files = glob('newspaper/data/*.{json}', GLOB_BRACE);
-    foreach($files as $file) {
-      $this->formatNewspaperData($file);
+    $numFiles = count($files);
+    foreach($files as $key => $file) {
+
+      if($key == 0) $divKey = 1;
+      else $divKey = $key;
+
+      $remaining = round(($divKey / $numFiles) * 100,2);
+      echo "Running file Newspaper - $key out of $numFiles ($remaining%)\n";
+
+      $progress = $this->checkProgress("newspaper",$key);
+
+      if($progress == 0) {
+        $this->formatNewspaperData($file);
+        $this->updateProgress("newspaper",$key);
+      }
     }
   }
 
-  private function formatJournalsData($data)
+  private function formatNewspaperData($data)
   {
     $json = file_get_contents($data);
     $data = json_decode($json,true);
-    // print_r($data);
 
     $arr = [];
     // loop articles in file
     foreach($data as $k => $v) {
 
-      $artKey = $v[0] . '_' . $v[1];
-      $date = $v[2];
+      $artKey = $v[0];
+      $date = $v[1];
 
-      foreach($v[4] as $peopleKey => $peopleVal) {
+      foreach($v[2] as $peopleKey => $peopleVal) {
         $arr[] = [
           'id' => $artKey,
           'date' => $date,
@@ -54,15 +79,6 @@ class IngestData {
     $this->parseFormatedData($arr);
   }
 
-  private function formatNewspaperData($data)
-  {
-    $json = file_get_contents($data);
-    $data = json_decode($json,true);
-    print_r($data);
-    die();
-  }
-
-
   private function parseFormatedData($data)
   {
     foreach($data as $k => $v) {
@@ -77,13 +93,45 @@ class IngestData {
     $data = [
       "add" => [
         "doc" => [
-          "id_s"          => $data['id'],
+          "art_id_s"      => $data['id'],
           "date_pdate"    => $data['date'],
           "name_t"        => $data['name'],
           "start_char_s"  => $data['start_char'],
           "end_char_s"    => $data['end_char'],
-          "qid_S"         => $data['qid'],
+          "qid_s"         => $data['qid'],
           "art_type_s"    => $data['art_type']
+        ],
+        "commitWithin" => 1000,
+      ],
+    ];
+    $data_string = json_encode($data);
+    
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+    curl_setopt($ch, CURLOPT_POST, TRUE);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-type: application/json'));
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
+    
+    $response = curl_exec($ch);    
+  }
+
+  private function checkProgress($type, $key)
+  {
+    $url = "http://localhost:8983/solr/amp/select?indent=true&q.op=OR&q=progress_s%3A%22{TYPE}_{KEY}%22";
+    $url = str_replace('{TYPE}',$type,$url);
+    $url = str_replace('{KEY}',$key,$url);
+    $responseJson = file_get_contents($url);
+    $response = json_decode($responseJson,true);
+    return $response['response']['numFound'];
+  }
+
+  private function updateProgress($type, $key)
+  {
+    $ch = curl_init("http://localhost:8983/solr/amp/update?wt=json");
+
+    $data = [
+      "add" => [
+        "doc" => [
+          "progress_s"  => "$type" ."_" ."$key",
         ],
         "commitWithin" => 1000,
       ],
@@ -97,8 +145,7 @@ class IngestData {
     
     $response = curl_exec($ch);
     
-    print_r($response);
-    
+    // print_r($response);
   }
 
 } 
